3b4303e4a3a2b2cf192fe438e2e52fb2148f1e49
wandyer.silva <wandyer.silva@crptecnologia.com.br>
2020-09-28 01:15:37 -0300

    react-app: refactored app layout

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
 import './App.css'
-import Home from "./components/home"
-import Navbar from './components/navbar'
+import Home from './components/Home'
+import Header from './components/Header'
+import Form from "./components/Form";
 
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
-import ResidenceForm from "./components/residenceForm";
 
 function App() {
     return (
         <Router>
-            <Navbar/>
-            <div className="container pt-4">
-                <div className="row">
-                    <div className="wrapper">
-                        <Switch>
-                            <Route path="/" component={Home} exact/>
-                            <Route path="/form" component={ResidenceForm}/>
-                            <Route component={Error}/>
-                        </Switch>
-                    </div>
-                </div>
-            </div>
+            <Header/>
+            <main role="main" className="container">
+                <Switch>
+                    <Route path="/" component={Home} exact/>
+                    <Route path="/form" component={Form}/>
+                    <Route component={Error}/>
+                </Switch>
+            </main>
         </Router>
     );
 }
